5b4ad9fbaab845c9dfc0c29d50c235bc4d261e50
Igor de Souza Bezerra <igorbezerra01@outlook.com>
2022-09-29 15:14:07 -0300

    testes para repository de produto

--- src/infrastructure/repository/product.repository.spec.ts
+++ src/infrastructure/repository/product.repository.spec.ts
@@ -1,4 +1,7 @@
 import { Sequelize } from "sequelize-typescript";
+import { Product } from "../../domain/entity/product";
+import { ProductModel } from "../db/sequelize/model/product.model";
+import { ProductRepository } from "./product.repository";
 
 describe("Product repository test", () => {
   let sequelize: Sequelize;
@@ -10,9 +13,63 @@ describe("Product repository test", () => {
       logging: false,
       sync: { force: true },
     });
+    sequelize.addModels([ProductModel]);
+    await sequelize.sync();
+  });
 
-    afterEach(async () => {
-      await sequelize.close();
+  afterEach(async () => {
+    await sequelize.close();
+  });
+  it("should create a product", async () => {
+    const productRepository = new ProductRepository();
+    const product = new Product("1", "Product 1", 100);
+
+    await productRepository.create(product);
+
+    const productModel = await ProductModel.findOne({ where: { id: "1" } });
+    expect(productModel.toJSON()).toStrictEqual({
+      id: "1",
+      name: "Product 1",
+      price: 100,
     });
+  });
+  it("should update a product", async () => {
+    const productRepository = new ProductRepository();
+    const product = new Product("1", "Product 1", 100);
+    await productRepository.create(product);
+    product.changeName("Product 2");
+    product.changePrice(200);
+
+    await productRepository.update(product);
+
+    const productModel = await ProductModel.findOne({ where: { id: "1" } });
+    expect(productModel.toJSON()).toStrictEqual({
+      id: "1",
+      name: "Product 2",
+      price: 200,
+    });
+  });
+  it("should find a product", async () => {
+    const productRepository = new ProductRepository();
+    const product = new Product("1", "Product 1", 100);
+    await productRepository.create(product);
+
+    const foundProduct = await productRepository.find("1");
+
+    expect(foundProduct.id).toBe(product.id);
+    expect(foundProduct.name).toBe(product.name);
+    expect(foundProduct.price).toBe(product.price);
+  });
+  it("should find all product", async () => {
+    const productRepository = new ProductRepository();
+    const product = new Product("1", "Product 1", 100);
+    await productRepository.create(product);
+    const product2 = new Product("2", "Product 2", 200);
+    await productRepository.create(product2);
+
+    const foundProducts = await productRepository.findAll();
+    const products = [product, product2];
+
+    expect(products).toEqual(foundProducts);
   });
 });
